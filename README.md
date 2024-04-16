cf6a9f728ea70d2aaa275856d0e3ad707e46bccd
luismart <luismart@live.cl>
2024-04-15 21:30:11 -0400

    iniciar servidor node

--- README.md
+++ README.md
@@ -21,3 +21,29 @@ No subir jamas la carpeta node modules:
 ```sh
 .gitignore
 ```
+
+## Iniciar servidor:
+
+Alternativa 1:
+```sh
+node index.js
+```
+
+2 Iniciar servidor con observable:
+```sh
+node --watch index.js
+```
+
+3 nodemon: es observar y reiniciar el servidor
+```sh
+npm i -D nodemon
+```
+
+json
+```sh
+  "scripts": {
+    "start": "node index.js",
+    "watch": "node --watch index.js",
+    "dev": "nodemon index.js"
+  },
+```
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hola mundo con Node.js');
+// Modulos commonjs node.js por defecto
+// const express = require('express') commonjs
+
+// se activa en package.json  "type": "module",
+import express from 'express' //modulos ES6 type module
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/hola', (req, res) => {
+    res.json({ msg: 'hola mundo con json' })
+})
+
+app.get('/chao', (req, res) => {
+    res.json({ msg: 'chao mundo con json' })
+})
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
